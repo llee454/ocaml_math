afad9f6682207e2d3e1c97c66af72796e44b30da
Larry Darryl Lee Jr <llee454@gmail.com>
2024-08-02 10:51:29 -0400

    Added the Route module and naive traveling salesperson algorithm.

--- src/ocaml_math.ml
+++ src/ocaml_math.ml
@@ -1083,10 +1083,9 @@ module FFT = struct
     let n = Array.length data in
     Array.init n ~f:(fun j ->
         Array.foldi data ~init:zero ~f:(fun k acc z ->
-            acc +
-              (Complex.from_polar
-                Complex.Polar.{ r = z; theta = Float.(-2.0 * pi * of_int j * of_int k / of_int n) })
-            ) )
+            acc
+            + Complex.from_polar
+                Complex.Polar.{ r = z; theta = Float.(-2.0 * pi * of_int j * of_int k / of_int n) } ) )
 
   let%expect_test "FFT.to_coeffs_slowly" =
     [| 0.0; 1.0; 0.0; 0.0 |] |> to_coeffs_slowly |> Complex.Rect.vector_to_string |> print_endline;
@@ -1595,10 +1594,12 @@ module Transitive_closure = struct
   (** represents the type of elements that will be grouped together *)
   module type Element = sig
     type t
+
     (**
       Represents the keys that elements have. These will be used to
       determine if two elements "overlap" and should be grouped together.
     *)
+
     module K : sig
       type t [@@deriving compare, equal, hash, sexp]
     end
@@ -1612,8 +1613,8 @@ module Transitive_closure = struct
   module Make (E : Element) = struct
     module KTable = Hashtbl.Make (E.K)
 
-    type t = E.t
     (** Accepts a list of elements and returns their transitive closure. *)
+    type t = E.t
 
     let get xs =
       let get_next_group_id =
@@ -1624,7 +1625,7 @@ module Transitive_closure = struct
           x
       in
       let key_group_tbl : int KTable.t = KTable.create () in
-      let group_keys_tbl :  E.Set.t Int.Table.t = Int.Table.create () in
+      let group_keys_tbl : E.Set.t Int.Table.t = Int.Table.create () in
       let group_records_tbl : E.t list Int.Table.t = Int.Table.create () in
 
       let make_empty_group () =
@@ -1658,7 +1659,8 @@ module Transitive_closure = struct
           let keys = E.get_keys x in
           let group_ids =
             Set.fold keys ~init:Int.Set.empty ~f:(fun group_ids k ->
-                Hashtbl.find_and_call ~if_found:(Set.add group_ids) ~if_not_found:(fun (_ : KTable.key) -> group_ids)
+                Hashtbl.find_and_call ~if_found:(Set.add group_ids)
+                  ~if_not_found:(fun (_ : KTable.key) -> group_ids)
                   key_group_tbl k )
             |> Set.to_sequence
           in
@@ -1673,7 +1675,6 @@ module Transitive_closure = struct
     type t = String.Set.t * int
 
     module K = String
-
     module Set = String.Set
 
     let get_keys = fst
@@ -1702,4 +1703,68 @@ module Transitive_closure = struct
       ((((B) 2)) (((A) 1)))
       ((((B) 2) ((A) 1) ((A B) 3)))
       ((((C) 4)) (((B) 2) ((A) 1) ((A B) 3))) |}]
+end
+
+module Route = struct
+  module Point = struct
+    type 'a t = {
+      label: 'a;
+      lat: float;
+      lon: float;
+    }
+    [@@deriving equal, sexp]
+
+    let dist p q =
+      let open Float in
+      sqrt (square (p.lat - q.lat) + square (p.lon - q.lon))
+  end
+
+  (**
+    Accepts two arguments: [start], a point; and [points], a list of
+    points; and returns the shortest path that starts with [start]
+    and passes through all of the points in [points].
+
+    WARNING: this function uses exhaustive depth-first search
+    and does not scale past a few points. Note however that this
+    function will return the absolute shortest path if it exists.
+
+    Example:
+
+    {[
+    let len, path =
+      find_path
+        ~label_equal:String.equal
+        Point.{ label = "3804 Delverne Rd, Baltimore, MD 21218"; lat = 39.33428; lon = -76.59854 }
+        Point.
+          [
+            { label = "1517 Tunlaw Rd, Baltimore, MD 21218"; lat = 39.33511; lon = -76.59328 };
+            { label = "3809 Monterey Rd, Baltimore, MD 21218"; lat = 39.33438; lon = -76.59716 };
+            { label = "1527 Tunlaw Rd, Baltimore, MD 21218"; lat = 39.33512; lon = -76.59287 };
+            { label = "3814 The Alameda, Baltimore, MD 21218"; lat = 39.33499; lon = -76.59688 };
+            { label = "1621 Ralworth Rd, Baltimore, MD 21218"; lat = 39.3392; lon = -76.59058 };
+            { label = "1533 Medford Rd, Baltimore, MD 21218"; lat = 39.33582; lon = -76.59241 };
+            { label = "3912 The Alameda, Baltimore, MD 21218"; lat = 39.33665; lon = -76.59888}
+          ]
+    in ...
+    ]}
+  *)
+  let find_path ~label_equal start points =
+    let rec aux curr_len last_point curr_path =
+      List.fold points ~init:None ~f:(fun acc point ->
+          if List.mem curr_path point ~equal:(Point.equal label_equal)
+          then acc
+          else (
+            let ((new_len, _) as new_res) =
+              aux Float.(curr_len + Point.dist last_point point) point (point :: curr_path)
+            in
+            match acc with
+            | None -> Some new_res
+            | Some (other_len, _) when Float.(new_len < other_len) -> Some new_res
+            | Some other -> Some other ) )
+      |> function
+      | None -> curr_len, curr_path
+      | Some res -> res
+    in
+    let len, path = aux 0.0 start (List.singleton start) in
+    len, List.rev path
 end
