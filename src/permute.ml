86acabf18d48f79bd7864f5987d1df0799c27e2c
Larry Darryl Lee Jr <llee454@gmail.com>
2026-06-14 21:06:08 -0400

    Added a function for computing all of the permutations of a set that may included repeated values - Permute.get_all_permutation_vecs.

--- src/permute.ml
+++ src/permute.ml
@@ -73,3 +73,221 @@ let gen_rand_binary_seq ~num_ones ~len () =
   |> Sequence.map ~f:get_expectation
   |> printf !"%{sexp: float Sequence.t}";
   [%expect {||}] *)
+
+(**
+  Represents information about a value that should appear in a permuted
+  list. Specifically, the number of times the value must appear.
+*)
+module Perm_value = struct
+  type 'a t = {
+    label: 'a;
+    mutable num: int
+  } [@@deriving fields, sexp]
+
+  let copy ~f x = { label = f x.label; num = x.num }
+end
+
+(**
+  Represents a partial permutation.
+*)
+module Partial_perm = struct
+  type 'a t = {
+    rem_vals: 'a Perm_value.t Fillable_vector.Tree.t;
+    perm: 'a list;
+  } [@@deriving sexp]
+
+  let extend ~(copy_label : 'a -> 'a) (x : 'a t) : 'a t Sequence.t =
+    let n = Fillable_vector.get_num_available x.rem_vals in
+    Sequence.init n ~f:(fun i ->
+      let rem_vals = Fillable_vector.Tree.copy ~f:(Perm_value.copy ~f:copy_label) x.rem_vals in
+      let xi = Fillable_vector.get_nth_unfilled i rem_vals
+        |> Option.value_exn ~here:[%here]
+      in
+      Fillable_vector.update ~unfilled_only:true
+        ~f:(fun info -> 
+          let open Perm_value in
+          info.num <- info.num - 1;
+          info)
+        ~is_full:(fun info -> [%equal: int] info.num 0)
+        i rem_vals;
+      { rem_vals; perm = List.cons xi.label x.perm }
+    )
+end
+
+(**
+  Accepts a set of values and returns the number of permutations that can
+  be formed using them.
+ *)
+let get_num_perms (values : 'a Perm_value.t array) : int =
+  Int.of_float @@
+    fact (Array.sum (module Int) values ~f:(fun value -> value.num)) /.
+    Array.fold values ~init:1.0 ~f:(fun acc value ->
+      acc *. (fact @@ Perm_value.num value)
+    )
+
+(**
+  Accepts a set of values and returns the set of all permutations of them.
+*)
+let get_all_permutation_vecs ~copy_label (values : 'a Perm_value.t array) =
+  let num_vals = Array.sum (module Int) ~f:Perm_value.num values in
+  if num_vals = 0
+  then [| [] |]
+  else begin
+    let perms = ref @@ Sequence.singleton
+      Partial_perm.{
+        rem_vals = Option.value_exn ~here:[%here] @@
+          Fillable_vector.create (Array.length values)
+            ~f:(fun i -> values.(i))
+            ~is_full:(fun _i info -> info.num = 0);
+        perm = []
+      }
+    in
+    for _i = 0 to num_vals - 1 do
+      perms := Sequence.concat_map !perms ~f:(fun perm -> Partial_perm.extend ~copy_label perm)
+    done;
+    !perms
+    |> Sequence.map ~f:(fun (x : 'a Partial_perm.t) -> x.perm)
+    |> Sequence.to_array
+  end
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [| |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     1
+     (())
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 0 };
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     1
+     (())
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 0 };
+      Perm_value.{ label = "B"; num = 0 };
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     1
+     (())
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 1 };
+      Perm_value.{ label = "B"; num = 0 };
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     1
+     ((A))
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 1 };
+      Perm_value.{ label = "B"; num = 1 };
+      Perm_value.{ label = "C"; num = 1 }
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     6
+     ((C B A) (B C A) (C A B) (A C B) (B A C) (A B C))
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 1 };
+      Perm_value.{ label = "B"; num = 2 };
+      Perm_value.{ label = "C"; num = 1 }
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     12
+     ((C B B A) (B C B A) (B B C A) (C B A B) (B C A B) (C A B B) (A C B B)
+     (B A C B) (A B C B) (B B A C) (B A B C) (A B B C))
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 3 };
+      Perm_value.{ label = "B"; num = 1 };
+      Perm_value.{ label = "C"; num = 1 }
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b\n %d\n %{sexp: string list array}\n" (num_perms = Array.length perms) num_perms perms;
+  [%expect {|
+    true
+     20
+     ((C B A A A) (B C A A A) (C A B A A) (A C B A A) (B A C A A) (A B C A A)
+     (C A A B A) (A C A B A) (A A C B A) (B A A C A) (A B A C A) (A A B C A)
+     (C A A A B) (A C A A B) (A A C A B) (A A A C B) (B A A A C) (A B A A C)
+     (A A B A C) (A A A B C))
+    |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 3 };
+      Perm_value.{ label = "B"; num = 5 };
+      Perm_value.{ label = "C"; num = 2 }
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b %d\n" (num_perms = Array.length perms) num_perms;
+  [%expect {| true 2520 |}]
+
+let%expect_test "get_all_permutation_vecs" =
+  let copy_label = String.of_string
+  and values = [|
+      Perm_value.{ label = "A"; num = 3 };
+      Perm_value.{ label = "B"; num = 5 };
+      Perm_value.{ label = "C"; num = 2 };
+      Perm_value.{ label = "D"; num = 0 }
+    |]
+  in
+  let num_perms = get_num_perms values in
+  let perms = get_all_permutation_vecs ~copy_label values in
+  printf !"%b %d\n" (num_perms = Array.length perms) num_perms;
+  [%expect {| true 2520 |}]
